a64fc96d140941b9c7dbeb51a596b6bd5b1fb379
juanrjara <jjaram@gmail.com>
2023-04-05 10:39:08 -0400

    newComponent addedto the project. It only contains a <p> label to be imported from app-root and also the css file was modified to show the text inred color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>My First Angular App</h1><app-new-component></app-new-component>',
+  styles: ['h1 { color: blue; text-align: center }']
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angulartest';
