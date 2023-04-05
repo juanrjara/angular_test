241b1acb7e2aef95f3071a0ac7461f42b94f2318
juanrjara <jjaram@gmail.com>
2023-04-05 11:04:47 -0400

    Added interfaces.ts file to src/app folder. Created Person interface in interfaces.ts file. Imported Person interface in new-component.component.ts file. Created a person object in new-component.component.ts file. Modified the person name property in new-component.component.ts file. Displayed the person name property in new-component.component.html file.

---
+++ src/app/interfaces.ts
@@ -0,0 +1,7 @@
+import { identifierModuleUrl } from "@angular/compiler";
+
+export interface Person {
+    name: string;
+    age: number;
+    id: number;
+}
--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../interfaces';
 
 @Component({
   selector: 'app-new-component',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-component.component.css']
 })
 export class NewComponentComponent implements OnInit {
+  newPerson1: Person = {
+    name: 'Juan',
+    age: 30,
+    id: 1
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.newPerson1.name += ' Andrés';
+    
   }
 
 }
